a22a4c8a1eb5ac92e351171de97486154afb9b60
Tom <tgadacy@gmail.com>
2021-10-10 15:46:49 -0400

    Remove unnessary images

--- src/main/java/com/killtimetracker/KillTimeTrackerPanel.java
+++ src/main/java/com/killtimetracker/KillTimeTrackerPanel.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
+import net.runelite.client.ui.components.PluginErrorPanel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +14,7 @@ public class KillTimeTrackerPanel extends PluginPanel
 {
     private final KillTimeTrackerPlugin plugin;
     private final JPanel panel = new JPanel();
+    private final PluginErrorPanel errorPanel = new PluginErrorPanel();
 
     public KillTimeTrackerPanel(KillTimeTrackerPlugin plugin)
     {
@@ -20,6 +22,8 @@ public class KillTimeTrackerPanel extends PluginPanel
         this.plugin = plugin;
         panel.setBackground(ColorScheme.DARK_GRAY_COLOR);
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        errorPanel.setContent("Kill Timers", "Could not load kill times");
+        panel.add(errorPanel);
         add(panel, BorderLayout.NORTH);
         panel.repaint();
         System.out.println("Is this working?");
